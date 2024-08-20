e3f2a8cc7a2a0e9eb4d12b1fa6ff8254bd16bf34
Iago Felipe <fe3ky07@gmail.com>
2024-08-20 16:13:46 -0300

    - Criação do restante dos modelos

--- vacinacao/models.py
+++ vacinacao/models.py
@@ -2,25 +2,50 @@ from django.db import models
 
 
 class Endereco(models.Model):
-    rua = models.CharField()
-    bairro = models.CharField()
-    numero = models.CharField()
-    cidade = models.CharField()
-    estado = models.CharField()
+    rua = models.CharField(max_length=50)
+    bairro = models.CharField(max_length=50)
+    numero = models.CharField(max_length=5)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50)
     cep = models.CharField(max_length=9)
 
 
 class Pessoa(models.Model):
-    nome = models.CharField()
+    nome = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11, unique=True)
     data_nascimento = models.DateField()
-    sexo = models.CharField(choices=(('M', 'Masculine'), ('F', 'Feminino')),)
+    sexo = models.CharField(choices=(('M', 'Masculino'), ('F', 'Feminino')), max_length=11)
     endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
 
 
-class Funcionario(Pessoa):
-    ...
+    def __str__(self):
+        return self.nome
 
+class Funcionario(Pessoa):
+    cnes = models.CharField(max_length=6)
+
+
+class Paciente(Pessoa):
+    sus = models.CharField(max_length=15)
+
+
+class Fabricante(models.Model):
+    nome = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.nome
+
+
+class Vacina(models.Model):
+    tipo = models.CharField(max_length=20)
+    lote = models.CharField(max_length=20)
+    validade = models.DateField()
+    quantidade = models.IntegerField()
+    fabricante = models.ForeignKey(Fabricante, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.tipo
